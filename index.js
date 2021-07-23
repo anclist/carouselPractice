dfb9cedc175c839eefed8f3447723a519ad6f50c
anclist <anclist@gmail.com>
2021-07-22 21:11:37 -0400

    Fixed Bug with the first and last slide

--- index.js
+++ index.js
@@ -33,22 +33,24 @@ let slidePosition = document.querySelector('.current-dot').dataset.index
 
 function moveToNextSlide() {
     removeClass()
-    if (slidePosition === totalSlides - 1) {
+    if (slidePosition == totalSlides - 1) {
         slidePosition = 0
     } else {
         slidePosition++
     }
+    console.log(slidePosition)
     dots[slidePosition].classList.toggle("current-dot")
     slides[slidePosition].classList.toggle("carousel-item-visible")
 }
 
 function moveToPrevSlide() {
     removeClass()
-    if ( slidePosition === 0) {
+    if ( slidePosition == 0) {
         slidePosition = totalSlides - 1
     } else {
         slidePosition--
     }
+    console.log(slidePosition)
     dots[slidePosition].classList.toggle("current-dot")
     slides[slidePosition].classList.toggle("carousel-item-visible")
 }
@@ -68,6 +70,7 @@ function moveSlide(item) {
     slidePosition = index
     dots[index].classList.toggle("current-dot")
     slides[index].classList.toggle("carousel-item-visible")
+    console.log(slidePosition)
 }
 
 function removeClass() {
